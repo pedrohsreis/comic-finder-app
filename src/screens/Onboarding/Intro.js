858918de843e143a36148633be5f7da432808f96
Pedro Reis <pedrohsareis@gmail.com>
2021-03-04 15:26:31 -0300

    fix: styling for intro page

--- src/screens/Onboarding/Intro.js
+++ src/screens/Onboarding/Intro.js
@@ -8,12 +8,17 @@ import {
 
 
 const Intro = () => {
-    
-    return(
-        <View style = { styles.root }>
-            <Image source = {require("../../assets/images/marvel-logo.png")} />
-            <Text>Welcome to the Marvel App!</Text>
-            <Text>Use this app as a tool to quickly find comics of your favorite hero!</Text>
+
+    return (
+        <View style={styles.root}>
+            <View style={styles.imageContainer}>
+                <Image source={require("../../assets/images/marvel-logo.png")} style={styles.image} />
+            </View>
+            <View style={styles.textContainer}>
+                <Text style = { styles.title }>Welcome to the Marvel App!</Text>
+                <Text style = { styles.description }>Use this app as a tool to quickly find comics of your favorite hero!</Text>
+                <Text style = { styles.description }>Hit next to start using!</Text>
+            </View>
         </View>
     )
 
@@ -22,9 +27,40 @@ const Intro = () => {
 const styles = StyleSheet.create({
     root: {
         flex: 1,
-        justifyContent: "center",
         alignItems: "center",
+        justifyContent: "center",
         backgroundColor: "#ed1d24"
+    },
+    imageContainer: {
+        width: "80%",
+        height: "15%",
+        margin: "15%",
+        marginLeft: "19%"
+    },
+    image: {
+        width: "100%",
+        height: "100%"
+    },
+    textContainer: {
+        width: "100%",
+        height: "50%",
+        backgroundColor: "#fff",
+        padding: "5%",
+        borderRadius: 20,
+        fontFamily: "Poppins-Medium",
+        top: 120
+    },
+    title: {
+        fontFamily: "Poppins-Medium",
+        textAlign: "center",
+        fontSize: 18,
+        color: "#ed1d24"
+    },
+    description: {
+        fontFamily: "Poppins-Regular",
+        fontSize: 15,
+        marginTop: "5%",
+        textAlign: "center"
     }
 })
 
